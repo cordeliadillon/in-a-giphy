c56869654e12e9c0a7353c8fddc5fca75505e3fd
Cordelia <cordelia.dillon@gmail.com>
2023-06-13 20:11:21 -0700

    Fix node issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import marko from "@marko/vite";
 
 export default defineConfig({
   plugins: [marko()],
-  base: '/in-a-giphy',
   build: {
     sourcemap: true, // Generate sourcemaps for all builds.
     emptyOutDir: false, // Avoid server & client deleting files from each other.
