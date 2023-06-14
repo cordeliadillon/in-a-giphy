a5029ecad3ebe085e30c641b865c3ab988454a50
Cordelia <cordelia.dillon@gmail.com>
2023-06-13 18:31:07 -0700

    Implement main MVP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import indexPage from "./pages/index";
-import usersService from "./services/users";
 
 export const router = Router()
-  .get("/", indexPage)
-  .get("/services/users", usersService);
+  .get("/", indexPage);
--- src/services/giphy/load.js
+++ src/services/giphy/load.js
@@ -1,20 +1,16 @@
 import { GIPHY_SEARCH_API_ENDPOINT } from "./constants";
 
 export const getGifs = async (query, options) => {
-  if (!query || query.length < 2) {
-    return;
-  }
-  const queryParams = new URLSearchParams({q: query, limit: 10, ...options}).toString();
+  const queryParams = new URLSearchParams({q: query, limit: 18, rating: 'g', ...options}).toString();
   const queryUrl = `${GIPHY_SEARCH_API_ENDPOINT}&${queryParams}`;
   console.log(queryUrl);
   try {
     const response = await fetch(queryUrl, {
       mode: 'cors',
-      headers: { "User-Agent": "chrome" },
     });
     const json = await response.json();
-    console.log(json);
-    return json.data;
+    console.log(json.data[0])
+    return json;
   } catch (error) {
     console.error(`Error: ${error}`);
   }
